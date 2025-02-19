e1fb84054584dd747d6564f9967f1b9a7e68c3d8
Subhadip Chakraborty <subhadip240420@gmail.com>
2025-02-19 11:19:52 +0530

    chacj

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Log;
 
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Carbon;
+use App\Models\User;
+
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -31,5 +35,25 @@ Route::get('view-logs', function () {
     return response($logs, 200)
              ->header('Content-Type', 'text/plain');
 });
+
+
+
+
+Route::get('/generate-verification-link/{id}', function ($id) {
+    $user = User::find($id);
+
+    if (!$user) {
+        return response()->json(['error' => 'User not found'], 404);
+    }
+
+    $link = URL::temporarySignedRoute(
+        'verification.verify',
+        Carbon::now()->addMinutes(60), // Link expires in 60 minutes
+        ['id' => $user->id, 'hash' => sha1($user->getEmailForVerification())]
+    );
+
+    return response()->json(['verification_link' => $link]);
+});
+
 
 require __DIR__.'/auth.php';
