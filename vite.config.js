bd463b48acce188ee7cd0cec20ee7081971b2bd9
Subhadip Chakraborty <subhadip240420@gmail.com>
2025-02-19 18:12:59 +0530

    chnage

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({command, mode }) => ({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: ['resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
