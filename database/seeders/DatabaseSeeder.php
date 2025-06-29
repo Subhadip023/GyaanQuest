063bf2ea2dde88694aeaf87edef4600547557ff6
Subhadip Chakraborty <subhadip240420@gmail.com>
2025-06-29 15:01:56 +0530

    chnake

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,34 +16,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@example.com',
-            'password' => bcrypt(12345678),
-            'email_verified_at' => now(),
-        ]);
+        // $user = User::factory()->create([
+        //     'name' => 'Admin',
+        //     'email' => 'admin@example.com',
+        //     'password' => bcrypt(12345678),
+        //     'email_verified_at' => now(),
+        // ]);
 
-        // Create Role
-        $role = Role::firstOrCreate(['name' => 'admin', 'guard_name' => 'web']);
+        // // Create Role
+        // $role = Role::firstOrCreate(['name' => 'admin', 'guard_name' => 'web']);
 
-        // Define Permissions
-        $permissions = [
-            'abc',
-            'create users',
-            'edit users',
-            'view users',
-            'view admin dashboard',
-            'test',
-        ];
+        // // Define Permissions
+        // $permissions = [
+        //     'abc',
+        //     'create users',
+        //     'edit users',
+        //     'view users',
+        //     'view admin dashboard',
+        //     'test',
+        // ];
 
-        // Create and Assign Permissions
-        foreach ($permissions as $p) {
-            $permission = Permission::firstOrCreate(['name' => $p, 'guard_name' => 'web']);
-            $role->givePermissionTo($permission);
-        }
+        // // Create and Assign Permissions
+        // foreach ($permissions as $p) {
+        //     $permission = Permission::firstOrCreate(['name' => $p, 'guard_name' => 'web']);
+        //     $role->givePermissionTo($permission);
+        // }
 
-        // Assign Role to Admin User
-        $user->assignRole('admin');
+        // // Assign Role to Admin User
+        // $user->assignRole('admin');
         User::factory(10)->create();
     }
     
